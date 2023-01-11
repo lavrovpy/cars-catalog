144a9f7f09590f017ebc98b61e44deb861cd83a2
alavreniuk <alavreniuk@exadel.com>
2023-01-11 16:03:41 +0200

    initital commit

---
+++ data.js
@@ -0,0 +1,57 @@
+export default [
+    {
+        name: 'Jeep',
+        url: 'https://www.jeep.com/',
+        image: './images/pexels-aaron-curtis-119435.jpg'
+    },
+    {
+        name: 'Dodge',
+        url: 'https://www.dodge.com/',
+        image: './images/pexels-garvin-st-villier-3311574.jpg'
+    },
+    {
+        name: 'Lada',
+        url: 'https://en.wikipedia.org/wiki/Lada',
+        image: './images/pexels-johnmark-smith-253096.jpg'
+    },
+    {
+        name: 'Mercedes',
+        url: 'https://www.mercedes-benz.com/en/',
+        image: './images/pexels-mike-b-112460.jpg'
+    },
+    {
+        name: 'BMW',
+        url: 'https://www.bmw.com/en/index.html',
+        image: './images/pexels-mike-b-170811.jpg'
+    },
+    {
+        name: 'Audi',
+        url: 'https://www.audi.com/en.html',
+        image: './images/pexels-mike-b-1035108.jpg'
+    },
+    {
+        name: 'Mercedes',
+        url: 'https://www.mercedes-benz.com/en/',
+        image: './images/pexels-mike-b-2365572.jpg'
+    },
+    {
+        name: 'Mini Cooper',
+        url: 'https://www.mini.com/en_MS/home.html',
+        image: './images/pexels-pixabay-35967.jpg'
+    },
+    {
+        name: 'Hyundai',
+        url: 'https://www.hyundai.com/worldwide/en/',
+        image: './images/pexels-s-von-hoerst-2920064.jpg'
+    },
+    {
+        name: 'BMW',
+        url: 'https://www.bmw.com/en/index.html',
+        image: './images/pexels-torsten-dettlaff-70912.jpg'
+    },
+    {
+        name: 'BMW',
+        url: 'https://www.bmw.com/en/index.html',
+        image: './images/pexels-trace-constant-707046.jpg'
+    }
+]
---
+++ index.js
@@ -0,0 +1,5 @@
+import data from './data.js'
+
+const appBody = document.getElementById('app')
+
+console.log('DATA', data)
