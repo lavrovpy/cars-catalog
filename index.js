efd56bf36af3aebd91879e2e1ecc1a6464f246f5
Andrii Lavreniuk <alo.andretti@gmail.com>
2023-01-11 20:06:15 +0200

    dynamically render cars

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-import data from './data.js'
+import cars from './data.js'
 
 const appBody = document.getElementById('app')
 
-console.log('DATA', data)
+const gallery = document.getElementById('cars')
+
+for(const car of cars){
+    const carConrainer = document.createElement('figure')
+    carConrainer.classList.add('gallery-item')
+
+    const carImage = document.createElement('img')
+    carImage.setAttribute('src', car.image)
+    carImage.setAttribute('alt', `This is the photo of ${car.name}`)
+    carImage.classList.add('gallery-image')
+    carConrainer.appendChild(carImage)
+
+    const a = document.createElement('a')
+    a.innerHTML = `<figcaption>${car.name}</figcaption>`;
+    a.setAttribute('href', car.url)
+    a.setAttribute('target', '_blank')
+    carConrainer.appendChild(a)
+
+    gallery.appendChild(carConrainer)
+}
