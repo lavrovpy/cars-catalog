7820499e5985e0b70c3ac9308e657fbc9cade6e4
Serhii <tsven4444@gmail.com>
2023-01-13 20:35:36 +0200

    implemented search feature

--- index.js
+++ index.js
@@ -4,35 +4,60 @@ const appBody = document.getElementById('app')
 
 const gallery = document.getElementById('cars')
 
-for(const car of cars){
+function renderCars(carsToRender){
+    for(const car of carsToRender){
 
-    const carConrainer = document.createElement('figure')
-    carConrainer.classList.add('gallery-item')
-
-    carConrainer.innerHTML = `
-        <a href="${car.url}">
-        
-        <img class="gallery-image" src="${car.image}" />
-        <figcaption>${car.name}</figcaption>
-        </a>
-`
-gallery.appendChild(carConrainer)
-
-    /* const carConrainer = document.createElement('figure')
-    carConrainer.classList.add('gallery-item')
-
-    const carImage = document.createElement('img')
-    carImage.setAttribute('src', car.image)
-    carImage.setAttribute('alt', `This is the photo of ${car.name}`)
-    carImage.classList.add('gallery-image')
-    carConrainer.appendChild(carImage)
-
-    const a = document.createElement('a')
-    a.innerHTML = `<figcaption>${car.name}</figcaption>`;
-    a.setAttribute('href', car.url)
-    a.setAttribute('target', '_blank')
-    carConrainer.appendChild(a)
-
-    gallery.appendChild(carConrainer)*/
+        const carConrainer = document.createElement('figure')
+        carConrainer.classList.add('gallery-item')
+    
+        carConrainer.innerHTML = `
+            <a href="${car.url}">
+            
+            <img class="gallery-image" src="${car.image}" />
+            <figcaption>${car.name}</figcaption>
+            </a>
+    `
+    gallery.appendChild(carConrainer)
+    
+    }
 }
+
+
+
+const searchBar = document.getElementById('search-box')
  
+searchBar.addEventListener('blur', hadleUserInput)
+
+searchBar.addEventListener('keyup', function(event){
+    console.log('here')
+    if(event.key === 'Enter'){
+        hadleUserInput(event)
+    }
+})
+
+function hadleUserInput(event){
+    const searchRequest = event.target.value;
+
+    if(searchRequest && searchRequest.lenght > 1){
+
+    
+    const filteredCars = cars.filter(car => {
+        return car.name.toLocaleLowerCase() === searchRequest.toLocaleLowerCase()});
+    removeElements(gallery)
+    renderCars(filteredCars)
+    } else{
+        removeElements(gallery)
+        renderCars(cars)
+    }
+}
+
+
+function removeElements(parentElement){
+    while(parentElement.lastChild){
+        parentElement.removeChild(parentElement.lastChild)
+    }
+
+    
+}
+
+renderCars(cars)
