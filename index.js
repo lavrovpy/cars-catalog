ecaf8dd0afe11892d5387a5d1bc45a8a57809061
 Богдан <bogdan@MacBook-Pro-Bogdan.local>
2023-01-13 19:14:19 +0200

    rendering cars with js

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-import data from './data.js'
+import cars from "./data.js";
 
-const appBody = document.getElementById('app')
+const appBody = document.getElementById("app");
 
-console.log('DATA', data)
+const gallery = document.getElementById("cars");
+
+for (const car of cars) {
+  const carContainer = document.createElement("figure");
+  carContainer.classList.add("gallery-item");
+
+  carContainer.innerHTML = `<a href="${car.url}">
+   <img
+     class="gallery-image"
+     src="${car.image}"
+   />
+   <figcaption>${car.name}</figcaption>
+ </a>`;
+
+  gallery.appendChild(carContainer);
+
+  // for (const car of cars) {
+  //   const carContainer = document.createElement("figure");
+  //   carContainer.classList.add("gallery-item");
+
+  //   const carImage = document.createElement("img");
+  //   carImage.setAttribute("src", car.image);
+  //   carContainer.appendChild(carImage);
+  //   carImage.setAttribute("alt", `This is the photo of ${car.name}`);
+  //   carImage.classList.add("gallery-image");
+
+  //   const a = document.createElement("a");
+  //   a.innerHTML = `<figcaption>${car.name}</figcaption>`;
+  //   a.setAttribute("href", car.url);
+  //   a.setAttribute("target", "_blank");
+  //   carContainer.appendChild(a);
+
+  //   gallery.appendChild(carContainer);
+}
