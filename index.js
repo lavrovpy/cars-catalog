cc9d1174e995738150b16566860a0979489fa11d
Andrii Lavreniuk <alo.andretti@gmail.com>
2023-01-21 15:00:11 +0200

    added year selector

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import data from './data.js'
 
-let cars = JSON.parse(JSON.stringify(data))
+let cars = JSON.parse(JSON.stringify(data));
 
 const appBody = document.getElementById('app')
+
+const allYears = data.map(function(car){
+    return car.year;
+})
+
+const uniqueYears = Array.from(new Set(allYears));
+/* for(const year of allYears){
+    if(!uniqueYears.includes(year)){
+        uniqueYears.push(year)
+    }
+} */
+
+const sortedYears = uniqueYears.sort((a, b) => {
+    return a - b
+})
 
 const gallery = document.getElementById('cars')
 
@@ -72,6 +87,31 @@ function deleteCar(event){
     const filteredCars = cars.filter(function(car){
         return car.id.toString() !== carIdToDelete;
     })
+    removeElements(gallery)
+    cars = filteredCars;
+    renderCars(cars)
+}
+
+const carYearSelector = document.querySelector('#car-selector')
+carYearSelector.addEventListener('change', handleYearSelection)
+
+function renderSelectorOptions(parentElement){
+    for(const year of sortedYears){
+        const option = document.createElement('option')
+        option.innerHTML = year
+        option.setAttribute('value', year)
+        parentElement.appendChild(option)
+    }
+}
+
+renderSelectorOptions(carYearSelector)
+
+function handleYearSelection(event){
+    debugger;
+    const selectedYear = event.target.value;
+    const filteredCars = data.filter(
+        car => car.year.toString() === selectedYear
+    )
     removeElements(gallery)
     cars = filteredCars;
     renderCars(cars)
